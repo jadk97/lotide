3da0499a9a63045ed8c20de9fccf7775bfd660d4
Jad <jad_dis@hotmail.com>
2019-09-16 15:33:47 -0400

    Update tail.js

--- tail.js
+++ tail.js
@@ -13,7 +13,3 @@ const tail = function(array) {
   }
   return result;
 };
-
-const words = ["Yo Yo", "Lighthouse", "Labs"];
-tail(words); // no need to capture the return value since we are not checking it
-assertEqual(words.length, 3); // original array should still have 3 elements!

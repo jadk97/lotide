612ac673f5a35dfdd61f5a84cfaea9def1452003
jad <jad_dis@hotmail.com>
2019-09-16 18:24:26 +0000

    Added head.js

--- head.js
+++ head.js
@@ -7,11 +7,9 @@ const assertEqual = function(actual, expected) {
 };
 
 
-const head = function(array){
+const head = function(array) {
   return array[0];
 };
 
 assertEqual(head([5,6,7]), 5);
 assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
-
-console.log(head([]));

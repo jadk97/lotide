80c918305ba60f2e4a0166b9d05012ee8c02616b
jad <jad_dis@hotmail.com>
2019-09-17 21:32:52 +0000

    Added flattened

---
+++ flatten.js
@@ -0,0 +1,68 @@
+const assertArraysEqual = function(array1, array2){
+  let equal = false;
+  if (array1.length === 0 && array1.length === 0){
+    equal = true;
+  }
+  else if(array1.length === array2.length){
+    for (let i = 0; i < array1.length; i++){
+      if(array1[i] === array2[i]){
+        equal = true;
+      }
+      else{
+        equal = false;
+        break;
+      }
+    }
+  }
+  else{
+    equal = false;
+  }
+  
+  if(equal){
+    console.log(`✅✅✅ Assertion Passed: ${array1} === ${array2}`)
+  }
+  else{
+    console.log(`🛑🛑🛑 Assertion Failed: ${array1} !==  ${array2}`);   
+  }
+};
+
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`🛑🛑🛑 Assertion Failed: ${actual} !==  ${expected}`);
+  }
+};
+
+
+const eqArrays = function(array1, array2){
+  let equal = false;
+  if(array1.length === array2.length){
+    for (let i = 0; i < array1.length; i++){
+      if(array1[i] === array2[i]){
+        equal = true;
+      }
+      else{
+        equal = false;
+        break;
+      }
+    }
+  }
+  else{
+    equal = false;
+  }
+  return equal;
+};
+
+const flatten = function(array){
+  let flattened = [];
+  for (let i = 0; i < array.length; i++){
+    if(Array.isArray(array[i])){
+      flattened.push.apply(flattened, array[i]);
+    }
+    else{
+      flattened.push(array[i]);
+    }
+  }
+  return flattened;
+};

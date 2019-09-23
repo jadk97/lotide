b13ca655355f52af77dd59de3981b94632e32b1f
jad <jad_dis@hotmail.com>
2019-09-23 17:52:11 +0000

    refactored middle and added middleTest

--- eqArrays.js
+++ eqArrays.js
@@ -1,6 +1,9 @@
 const eqArrays = function(array1, array2){
   let equal = false;
-  if(array1.length === array2.length){
+  if(array1.length === 0 && array2.length === 0){
+    equal = true;
+  }
+  else if(array1.length === array2.length){
     for (let i = 0; i < array1.length; i++){
       if(array1[i] === array2[i]){
         equal = true;
--- middle.js
+++ middle.js
@@ -1,32 +1,3 @@
-const assertArraysEqual = function(array1, array2){
-  let equal = false;
-  if (array1.length === 0 && array1.length === 0){
-    equal = true;
-  }
-  else if(array1.length === array2.length){
-    for (let i = 0; i < array1.length; i++){
-      if(array1[i] === array2[i]){
-        equal = true;
-      }
-      else{
-        equal = false;
-        break;
-      }
-    }
-  }
-
-  else{
-    equal = false;
-  }
-  
-  if(equal){
-    console.log(`✅✅✅ Assertion Passed: ${array1} === ${array2}`)
-  }
-  else{
-    console.log(`🛑🛑🛑 Assertion Failed: ${array1} !==  ${array2}`);   
-  }
-};
-
 const middle = function (array){
   let middleArray = [];
   let middleElement = Math.ceil(array.length/2);
@@ -45,9 +16,5 @@ const middle = function (array){
 };
 
 module.exports = middle;
-assertArraysEqual(middle([1]), []);
-assertArraysEqual(middle([1, 2]), []);
-assertArraysEqual(middle([1, 2, 3]), [2]);
-assertArraysEqual(middle([1, 2, 3, 4]), [2, 3]);
-assertArraysEqual(middle([1, 2, 3, 4, 5]), [3]);
-assertArraysEqual(middle([1, 2, 3, 4, 5, 6]), [3, 4]);
+
+
